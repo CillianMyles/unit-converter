6a27957c74db37d9e0355855fa22844399495599
Cillian Myles <mylsey4thewin@gmail.com>
2019-03-26 19:16:23 +0100

    Show list of unit categories;

--- lib/category_screen.dart
+++ lib/category_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:unit_converter/category.dart';
 
 const _appName = 'Unit Converter';
 final _bgColor = Colors.green[100];
+const _defaultIcon = Icons.cake;
 
 class CategoryScreen extends StatelessWidget {
   const CategoryScreen();
@@ -28,21 +30,46 @@ class CategoryScreen extends StatelessWidget {
     Colors.red,
   ];
 
+  Widget _buildCategoryWidgets(List<Widget> categories) {
+    return ListView.builder(
+      itemBuilder: (BuildContext context, int index) => categories[index],
+      itemCount: categories.length,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: _bgColor,
-        title: Text(
+    final appBar = AppBar(
+      backgroundColor: _bgColor,
+      elevation: 0.0,
+      title: Center(
+        child: Text(
           _appName,
           style: TextStyle(
             fontSize: 30.0,
           ),
         ),
       ),
-      body: Container(
-        color: _bgColor,
-      ),
+    );
+
+    final categories = <Category>[];
+    for (var i = 0; i < _categoryNames.length; i++) {
+      categories.add(Category(
+        name: _categoryNames[i],
+        color: _baseColours[i],
+        iconLocation: _defaultIcon,
+      ));
+    }
+
+    final listView = Container(
+      color: _bgColor,
+      padding: EdgeInsets.symmetric(horizontal: 8.0),
+      child: _buildCategoryWidgets(categories),
+    );
+
+    return Scaffold(
+      appBar: appBar,
+      body: listView,
     );
   }
 }
